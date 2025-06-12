4f60225b12fb19f071b435bd27d96c21c69ad059
FrogePerson <129311808+FrogePerson@users.noreply.github.com>
2025-06-12 18:51:10 +0300

    Добавил тестовую машинку

--- Assets/Scripts/Car/Spin.cs
+++ Assets/Scripts/Car/Spin.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class Spin : MonoBehaviour
 {
 
-    public float rotationSpeed = 1f;
-    private Rigidbody rb;
+    public static float rotationSpeed = 6f;
+
+    [SerializeField] private GameObject Wheel0;
+    [SerializeField] private GameObject Wheel1;
+
+    Rigidbody rb;
+
+    Rigidbody rbWheel0;
+    Rigidbody rbWheel1;
+    Rigidbody rbWheel2;
+    Rigidbody rbWheel3;
+    float counter = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        rbWheel0 = Wheel0.GetComponent<Rigidbody>();
+        rbWheel1 = Wheel0.GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
+        rotationSpeed+=counter;
+        counter+=0.005f;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, -rotationSpeed, 0));
+        Vector3 force = transform.forward * (counter+50f);
 
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, rotationSpeed, 0));
+        if(rotationSpeed >= 50)
+        {
+            rotationSpeed = 50;
+        }
+        rbWheel0.AddForce(force, ForceMode.Force);
+        rbWheel1.AddForce(force, ForceMode.Force);
     }
 }
